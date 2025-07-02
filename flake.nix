58d183618e57532fd859207e33e07de6e4e6cbdc
Andrew Jeffery <dev@jeffas.net>
2025-07-02 07:53:47 +0100

    Add cargo nix build

--- flake.nix
+++ flake.nix
@@ -11,12 +11,18 @@
   }: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
+    cargoNix = pkgs.callPackage ./Cargo.nix {};
+    packages = pkgs.lib.mapAttrs (name: crate: crate.build) cargoNix.workspaceMembers;
   in {
+    packages.${system} = packages;
+
     devShells.${system}.default = pkgs.mkShell {
       packages = [
         pkgs.rustc
         pkgs.cargo
         pkgs.rustfmt
+
+        pkgs.crate2nix
       ];
     };
   };
